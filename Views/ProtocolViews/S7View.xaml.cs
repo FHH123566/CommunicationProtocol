de7acb3ec946a6b03c0ffad119e95a3053bd1136
FHH123566 <13918384990@163.com>
2026-05-10 18:34:39 +0800

    Create S7View.xaml.cs

---
+++ Views/ProtocolViews/S7View.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace CommunicationProtocol.WPF.Views.ProtocolViews
+{
+public partial class S7View : UserControl
+{
+public S7View()
+{
+InitializeComponent();
+}
+}
+}
