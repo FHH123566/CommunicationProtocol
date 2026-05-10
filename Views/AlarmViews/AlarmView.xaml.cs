5637412f1d1dd10cfa74bc0c2e1dbb3b648d4e0e
FHH123566 <13918384990@163.com>
2026-05-10 18:46:32 +0800

    Create AlarmView.xaml.cs

---
+++ Views/AlarmViews/AlarmView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace CommunicationProtocol.WPF.Views.AlarmViews
+{
+public partial class AlarmView : UserControl
+{
+public AlarmView()
+{
+InitializeComponent();
+}
+}
+}
