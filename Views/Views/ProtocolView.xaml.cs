f0a985e083c342e34effe5f6e75d6516f34aeaff
FHH123566 <13918384990@163.com>
2026-05-10 16:24:29 +0800

    Create ProtocolView.xaml.cs

---
+++ Views/Views/ProtocolView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace CommunicationProtocol.WPF.Views
+{
+public partial class ProtocolView : UserControl
+{
+public ProtocolView()
+{
+InitializeComponent();
+}
+}
+}
