68070142fc7a737c80584aa1d5eb625c527fe3d4
FHH123566 <13918384990@163.com>
2026-05-10 18:45:40 +0800

    Create RealTimeMonitorView.xaml.cs

---
+++ Views/MonitorViews/RealTimeMonitorView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace CommunicationProtocol.WPF.Views.MonitorViews
+{
+public partial class RealTimeMonitorView : UserControl
+{
+public RealTimeMonitorView()
+{
+InitializeComponent();
+}
+}
+}
