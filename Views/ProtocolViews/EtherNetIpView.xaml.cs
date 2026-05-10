d2c1340bca4d7d8f2ac594aa130a13a138cf18e4
FHH123566 <13918384990@163.com>
2026-05-10 18:44:48 +0800

    Create EtherNetIpView.xaml.cs

---
+++ Views/ProtocolViews/EtherNetIpView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace CommunicationProtocol.WPF.Views.ProtocolViews
+{
+public partial class EtherNetIpView : UserControl
+{
+public EtherNetIpView()
+{
+InitializeComponent();
+}
+}
+}
