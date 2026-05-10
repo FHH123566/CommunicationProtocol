789ad674c46ff4f3865a98083127f802afffaf37
FHH123566 <13918384990@163.com>
2026-05-10 18:33:29 +0800

    Create ModbusView.xaml.cs

---
+++ Views/ProtocolViews/ModbusView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace CommunicationProtocol.WPF.Views.ProtocolViews
+{
+public partial class ModbusView : UserControl
+{
+public ModbusView()
+{
+InitializeComponent();
+}
+}
+}
